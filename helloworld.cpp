85752ab92f4b7b8ee9717c4c10e287ab49cc7422
AyaanG123 <160043446+AyaanG123@users.noreply.github.com>
2024-09-10 09:22:50 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+int a = 4;
+float b = 4.5;
+char c = 'a';
+bool f = false;
+std:: string s = "sigma";
+    std::cout << "integer a is equal to " << a << " and float b is equal to " << b << " and char c is equal to " << c << " and bool f is equal to "<< f << " and finally, string s is equal to " << s << std::endl;
+}
